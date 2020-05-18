300e97824f427ff6253e1953e2c881c92ed10acd
Hyounguk Shon <hyounguk.shon@gmail.com>
2020-05-19 01:01:34 +0900

    initial commit

---
+++ dataloader/dataloader.py
@@ -0,0 +1,16 @@
+from torch.utils.data import Dataset, DataLoader
+
+
+class DataSubset(Dataset):
+    def __init__(self, base_dataset, inds=None, size=-1):
+        self.base_dataset = base_dataset
+        if inds is None:
+            inds = np.random.choice(list(range(len(base_dataset))), size, replace=False)
+        self.inds = inds
+
+    def __getitem__(self, index):
+        base_ind = self.inds[index]
+        return self.base_dataset[base_ind]
+
+    def __len__(self):
+        return len(self.inds)
---
+++ model/model.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+import .model.wideresnet as wideresnet
+
+class F(nn.Module):
+    def __init__(self, depth=28, width=2, norm=None, dropout_rate=0.0, n_classes=10):
+        super(F, self).__init__()
+        self.f = wideresnet.Wide_ResNet(depth, width, norm=norm, dropout_rate=dropout_rate)
+        self.energy_output = nn.Linear(self.f.last_dim, 1)
+        self.class_output = nn.Linear(self.f.last_dim, n_classes)
+
+    def forward(self, x, y=None):
+        penult_z = self.f(x)
+        return self.energy_output(penult_z).squeeze()
+
+    def classify(self, x):
+        penult_z = self.f(x)
+        return self.class_output(penult_z).squeeze()
+
+
+class CCF(F):
+    def __init__(self, depth=28, width=2, norm=None, dropout_rate=0.0, n_classes=10):
+        super(CCF, self).__init__(depth, width, norm=norm, dropout_rate=dropout_rate, n_classes=n_classes)
+
+    def forward(self, x, y=None):
+        logits = self.classify(x)
+        if y is None:
+            return logits.logsumexp(1)
+        else:
+            return t.gather(logits, 1, y[:, None])
